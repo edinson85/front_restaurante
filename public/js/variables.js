7e1de185516acf5b1a8f8378b4fe3a6d64f0f468
Edinson Solarte <edsolarte85@gmail.com>
2024-03-12 15:45:19 -0500

    Se ajustan pestañas

--- public/js/variables.js
+++ public/js/variables.js
@@ -1,6 +1,6 @@
-var urlGerente = "http://localhost:8082/";
-var urlCocina = "http://localhost:8084/";
-var urlBodega = "http://localhost:8086/";
+var urlGerente = "http://52.71.174.39:8000/";
+var urlCocina = "http://52.55.45.106:8000/";
+var urlBodega = "http://44.221.174.235:8000/";
 function getUrlGerente(){
     return urlGerente;
 }
